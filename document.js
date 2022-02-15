8ff8ff808525795390682dec1f8c3b75f0fee61e
Sahil Padvi <sahilpadvi25@gmail.com>
2022-02-15 16:08:07 +0530

    style: add style to modal

--- document.js
+++ document.js
@@ -1,3 +1,5 @@
+const body = document.querySelector('body');
+
 /* Snackbar js */
 const snackbarLeftBtn = document.querySelector('.snackbar-left-btn');
 
@@ -5,7 +7,7 @@ const snackbarRightBtn = document.querySelector('.snackbar-right-btn');
 
 const snackbarCenterBtn = document.querySelector('.snackbar-center-btn');
 
-const snackbar = document.querySelector('.snackbar-demo');
+const snackbar = document.querySelector('.snackbar-container');
 
 snackbarCenterBtn.addEventListener('click', () => {
   displaySnackbar('snackbar-center');
@@ -27,3 +29,19 @@ function displaySnackbar(snackpos) {
     snackbar.classList.remove(snackpos);
   }, 3000);
 }
+
+/* Modal */
+//const modalImageBtn = document.querySelector('');
+const modalBtn = document.querySelector('.modal-btn');
+const modalCloseBtn = document.querySelector('.modal-close-btn');
+const modal = document.querySelector('.modal-container');
+
+modalCloseBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+  body.classList.remove('modal-open');
+});
+
+modalBtn.addEventListener('click', () => {
+  modal.style.display = 'block';
+  body.classList.add('modal-open');
+});
